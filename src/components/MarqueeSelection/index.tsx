f125b9308678942e35b7b5bfbef9918ca9f7e3b9
Weil Liao <liaoweil05621@gmail.com>
2024-08-21 17:28:14 +0800

    fix(MarqueeSelection): improve cursor styling

--- src/components/MarqueeSelection/index.tsx
+++ src/components/MarqueeSelection/index.tsx
@@ -22,14 +22,13 @@ import SelectionArea from "./SelectionArea";
 
 const START_ID_ORDER = 1 as SelectionId;
 const CORNER_SQUARE_SIZE = 6;
+const EXTRA_RESIZE_INTERACTION_SPACE = CORNER_SQUARE_SIZE * 1.5;
 
 interface MarqueeSelectionProps {
   containerHeight: number;
   constrainX: (x: number) => number;
   constrainY: (y: number) => number;
 }
-
-// FIX: improve cursor styling
 
 /**
  * @description this component is heavily depended on currentSelection & selections,
@@ -43,6 +42,7 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
   const [currentSelection, setCurrentSelection] = useState<Selection | null>(
     null,
   );
+  const [activeCursor, setActiveCursor] = useState<string | null>(null);
 
   const selectionContainerRef = useRef<HTMLDivElement>(null);
   const selectionContainerBoundingClientRect = useRef<{
@@ -73,6 +73,7 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
     e: MouseEvent<HTMLDivElement>,
     id?: SelectionId,
     direction?: DIRECTION,
+    cursor?: string,
   ) => {
     if (e.target !== e.currentTarget) return;
     shouldMousemoveActivate.current = true;
@@ -85,9 +86,10 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
     const startY = e.clientY - top;
 
     // only existed selection has id & adjusting direction
-    if (direction && id) {
+    if (direction && id && cursor) {
       resizeDirection.current = direction;
       activeSelectionId.current = id;
+      setActiveCursor(cursor);
       return;
     }
 
@@ -162,7 +164,6 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
       isSelectionOverlapping(selectionForOverlappingFeedback)
     );
   };
-
   const handleMouseUp = () => {
     if (
       preEditSelection.current &&
@@ -177,6 +178,7 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
     }
 
     setCurrentSelection(null);
+    setActiveCursor(null);
     shouldMousemoveActivate.current = false;
     isOverlapping.current = false;
     activeSelectionId.current = null;
@@ -224,48 +226,78 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
         top: selection.startY - CORNER_SQUARE_SIZE / 2,
         left: selection.startX - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.TOP_LEFT,
+        width: EXTRA_RESIZE_INTERACTION_SPACE,
+        height: EXTRA_RESIZE_INTERACTION_SPACE,
       },
       {
         cursor: cornerCursors[1],
         top: selection.startY - CORNER_SQUARE_SIZE / 2,
         left: selection.endX - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.TOP_RIGHT,
+        width: EXTRA_RESIZE_INTERACTION_SPACE,
+        height: EXTRA_RESIZE_INTERACTION_SPACE,
       },
       {
         cursor: cornerCursors[1],
         top: selection.endY - CORNER_SQUARE_SIZE / 2,
         left: selection.startX - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.BOTTOM_LEFT,
+        isAlign: true,
+        width: EXTRA_RESIZE_INTERACTION_SPACE,
+        height: EXTRA_RESIZE_INTERACTION_SPACE,
       },
       {
         cursor: cornerCursors[0],
         top: selection.endY - CORNER_SQUARE_SIZE / 2,
         left: selection.endX - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.BOTTOM_RIGHT,
+        isAlign: true,
+        width: EXTRA_RESIZE_INTERACTION_SPACE,
+        height: EXTRA_RESIZE_INTERACTION_SPACE,
       },
       {
         cursor: DEFAULT_RESIZE_CURSORS[0],
         top: selection.startY - CORNER_SQUARE_SIZE / 2,
         left: (selection.startX + selection.endX) / 2 - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.TOP,
+        isAlign: false,
+        width:
+          Math.abs(selection.startX - selection.endX) -
+          EXTRA_RESIZE_INTERACTION_SPACE * 2,
+        height: EXTRA_RESIZE_INTERACTION_SPACE,
       },
       {
         cursor: DEFAULT_RESIZE_CURSORS[1],
         top: (selection.startY + selection.endY) / 2 - CORNER_SQUARE_SIZE / 2,
         left: selection.endX - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.RIGHT,
+        isAlign: false,
+        width: EXTRA_RESIZE_INTERACTION_SPACE,
+        height:
+          Math.abs(selection.startY - selection.endY) -
+          EXTRA_RESIZE_INTERACTION_SPACE * 2,
       },
       {
         cursor: DEFAULT_RESIZE_CURSORS[0],
         top: selection.endY - CORNER_SQUARE_SIZE / 2,
         left: (selection.startX + selection.endX) / 2 - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.BOTTOM,
+        isAlign: false,
+        width:
+          Math.abs(selection.startX - selection.endX) -
+          EXTRA_RESIZE_INTERACTION_SPACE * 2,
+        height: EXTRA_RESIZE_INTERACTION_SPACE,
       },
       {
         cursor: DEFAULT_RESIZE_CURSORS[1],
         top: (selection.startY + selection.endY) / 2 - CORNER_SQUARE_SIZE / 2,
         left: selection.startX - CORNER_SQUARE_SIZE / 2,
         direction: DIRECTION.LEFT,
+        isAlign: false,
+        width: EXTRA_RESIZE_INTERACTION_SPACE,
+        height:
+          Math.abs(selection.startY - selection.endY) -
+          EXTRA_RESIZE_INTERACTION_SPACE * 2,
       },
     ];
 
@@ -275,18 +307,18 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
         : DEFAULT_SELECTION_COLOR;
 
     return positions.map((pos, index) => (
-      <div
+      <ResizeSquare
         key={`${id}-${index}`}
-        onMouseDown={(e) => handleMouseDown(e, id, pos.direction)}
+        $direction={pos.direction}
+        $height={pos.height}
+        $width={pos.width}
+        $cursor={activeCursor ?? pos.cursor}
+        onMouseDown={(e) => handleMouseDown(e, id, pos.direction, pos.cursor)}
         style={{
           position: "absolute",
           width: CORNER_SQUARE_SIZE,
           height: CORNER_SQUARE_SIZE,
           backgroundColor: bgColor,
-          cursor:
-            isHoveringExistedSelection.current && activeSelectionId
-              ? "crosshair"
-              : pos.cursor,
           top: pos.top,
           left: pos.left,
         }}
@@ -302,7 +334,7 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       style={{
-        cursor: "crosshair",
+        cursor: activeCursor ?? "crosshair",
       }}
     >
       {selections.map((selection, index) => (
@@ -310,19 +342,15 @@ const MarqueeSelection = (props: MarqueeSelectionProps) => {
           <SelectionArea
             onMouseOver={() => {
               isHoveringExistedSelection.current = true;
-              requestAnimationFrame(() => {
-                selectionContainerRef.current!.style.cursor =
-                  isOverlapping.current ? "crosshair" : "grab";
-              });
             }}
             onMouseOut={() => {
               isHoveringExistedSelection.current = false;
-              selectionContainerRef.current!.style.cursor = "crosshair";
             }}
             index={index + 1}
             onDelete={() => {
               deleteSelection(selection.id);
             }}
+            disabled={!!activeCursor}
             selection={selection}
             isOverlapping={
               isOverlapping.current &&
@@ -349,6 +377,45 @@ const SelectionContainer = styled.div<{ $height: number }>`
   width: 100%;
   height: ${(props) => props.$height}px;
   position: absolute;
+`;
+
+const ResizeSquare = styled.div<{
+  $width: number;
+  $height: number;
+  $direction: DIRECTION;
+  $cursor: string;
+}>`
+  cursor: ${(props) => props.$cursor};
+  &::after {
+    content: "";
+    display: block;
+    cursor: inherit;
+    width: ${(props) => props.$width}px;
+    height: ${(props) => props.$height}px;
+    margin: ${(props) => {
+      const mx = (props.$width - EXTRA_RESIZE_INTERACTION_SPACE) / 2;
+      const my = (props.$height - EXTRA_RESIZE_INTERACTION_SPACE) / 2;
+
+      switch (props.$direction) {
+        case DIRECTION.TOP: {
+          return `0 0 0 -${mx}px`;
+        }
+        case DIRECTION.BOTTOM: {
+          return `0 0 0 -${mx}px`;
+        }
+        case DIRECTION.LEFT: {
+          return `-${my}px 0 0 0`;
+        }
+        case DIRECTION.RIGHT: {
+          return `-${my}px 0 0 0`;
+        }
+
+        default:
+          return 0;
+      }
+    }};
+    background-color: transparent;
+  }
 `;
 
 export default MarqueeSelection;
